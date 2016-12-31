8758b0a870f36422f9beca9c325941aa96259a97
Kamil Naja <kamilnaja@gmail.com>
2016-12-31 19:32:06 +0100

    adding routes for menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DogComponent } from './dog/dog.component';
+import { PuppydetailsComponent } from './puppydetails/puppydetails.component';
+import { MenuComponent } from './menu/menu.component';
+
+const appRoutes: Routes = [
+{  path: 'puppy-details', component: PuppydetailsComponent }];
 
 @NgModule({
   declarations: [
     AppComponent,
-    DogComponent
+    DogComponent,
+    PuppydetailsComponent,
+    MenuComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [DogComponent],
-  bootstrap: [AppComponent, DogComponent]
+  providers: [DogComponent, PuppydetailsComponent, MenuComponent],
+  bootstrap: [AppComponent, DogComponent, PuppydetailsComponent, MenuComponent]
 })
-export class AppModule { }
+export class AppModule { 
+}
---
+++ src/app/puppydetails/puppydetails.component.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-puppydetails',
+  templateUrl: './puppydetails.component.html',
+  styleUrls: ['./puppydetails.component.css']
+})
+export class PuppydetailsComponent {
+
+  constructor() { }
+
+}
