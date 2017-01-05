dadd6a3b67e485c4d1c23d64b3f3468fe5fc16e0
Kamil Naja <kamilnaja@gmail.com>
2017-01-05 21:02:55 +0100

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{383:function(n,o){},694:function(n,o,c){n.exports=c(383)}},[694]);
+webpackJsonp([1,2],{383:function(n,o){},696:function(n,o,c){n.exports=c(383)}},[696]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
